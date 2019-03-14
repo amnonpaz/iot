d1dbb300fa9436ef730dddf2e8850f2fa6bef263
Amnon Paz <pazamnon@gmail.com>
2019-03-14 12:04:14 +0200

    Scripts: Implement pairing class

---
+++ scripts/pairing.py
@@ -0,0 +1,70 @@
+import socket
+import struct
+
+
+class PairingClient:
+    socketTimeout = 5.0
+    readBuffSize = 16
+
+    ssidLen = 32
+    passwdLen = 64
+
+    messageTypeNull = 0
+    messageTypeError = 1
+    messageTypePair = 2
+    messageTypeAck = 3
+
+    def __init__(self, pairing_server_ip = '192.168.0.1', pairing_port = 6001):
+        self.pairingServerIp = pairing_server_ip
+        self.pairingPort = pairing_port
+        self.connected = False
+        self.sock = None
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(self.socketTimeout)
+        try:
+            self.sock.connect((self.pairingServerIp, self.pairingPort))
+            self.connected = True
+        except Exception as e:
+            print("Error connecting to pairing server: ", e)
+
+        return self.connected
+
+    def disconnect(self):
+        self.sock.close()
+        self.connected = False
+
+
+    def send_pair_message(self, ssid, passwd):
+        if not self.connected:
+            print("Cannot send message without server connection")
+            return False
+
+        try:
+            self.sock.send(self.prepare_pairing_message(ssid, passwd))
+        except Exception as e:
+            print("Error sending pairing message: ", e)
+            return False
+
+        return True
+
+    def prepare_pairing_message(self, ssid, passwd):
+        return struct.pack("L" + str(self.ssidLen) + "s" + str(self.passwdLen) + "s",
+                           self.messageTypePair, ssid.encode(), passwd.encode())
+
+    def read_reply(self):
+        reply = None
+        try:
+            reply = self.sock.recv(self.readBuffSize)
+        except Exception as e:
+            print("Error receiving reply: ", e)
+
+        if reply is None:
+            return None
+
+        return self.parse_reply(reply)
+
+    def parse_reply(self, reply_buff):
+        return struct.unpack("L", reply_buff)[0]
+
