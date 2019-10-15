2da9edbed04f419927268486e879e459554fba51
Amnon Paz <pazamnon@gmail.com>
2019-10-15 23:57:22 +0300

    data-collector: Re-indent MqttClient.?pp

--- server-side/data-collector/src/MqttClient.cpp
+++ server-side/data-collector/src/MqttClient.cpp
@@ -6,80 +6,80 @@
 
 namespace comm {
 
-void MqttClient::SharedResources::up()
-{
-    std::unique_lock lk(m_lock);
+    void MqttClient::SharedResources::up()
+    {
+        std::unique_lock lk(m_lock);
 
-    if (m_count == 0)
-        mosqpp::lib_init();
+        if (m_count == 0)
+            mosqpp::lib_init();
 
-    m_count++;
-}
-
-void MqttClient::SharedResources::down()
-{
-    std::unique_lock lk(m_lock);
-
-    m_count--;
-
-    if (m_count == 0)
-        mosqpp::lib_cleanup();
-
-}
-
-MqttClient::SharedResources MqttClient::s_sharedResources; 
-
-MqttClient::MqttClient(std::string brokerUrl,
-                       uint16_t brokerPort,
-                       std::string clientId) :
-    mosquittopp{clientId.c_str()},
-    m_brokerAddress({std::move(brokerUrl), brokerPort}),
-    m_state{StateInit} {
-    s_sharedResources.up();
-}
-
-MqttClient::~MqttClient() {
-    s_sharedResources.down();
-}
-
-void MqttClient::connect() {
-    int rc;
-    if (m_state == StateDisconnected) {
-        rc = mosquittopp::reconnect();
-    } else {
-        rc = mosquittopp::connect_async(std::get<0>(m_brokerAddress).c_str(),
-                                        std::get<1>(m_brokerAddress),
-                                        s_keepAliveS);
+        m_count++;
     }
 
-    if (rc) {
-        printErrorMessage(rc);
-        reconnect();
+    void MqttClient::SharedResources::down()
+    {
+        std::unique_lock lk(m_lock);
+
+        m_count--;
+
+        if (m_count == 0)
+            mosqpp::lib_cleanup();
+
     }
-}
 
-void MqttClient::reconnect() {
-    std::cerr << "Trying to reconnect in "
-              << s_reconnectTimeoutS << " seconds\n";
-    utils::TimedAsyncExecution(s_reconnectTimeoutS*1000,
-                               [this](){ connect(); });
-}
+    MqttClient::SharedResources MqttClient::s_sharedResources;
 
-void MqttClient::on_connect(int rc) {
-    if (!rc) {
-        std::cout << "Connected...\n";
-        m_state = StateConnected;
-    } else {
-        printErrorMessage(rc);
-        if (m_state != StateInit)
-            m_state = StateDisconnected;
-
-        reconnect();
+    MqttClient::MqttClient(std::string brokerUrl,
+                           uint16_t brokerPort,
+                           std::string clientId) :
+        mosquittopp{clientId.c_str()},
+        m_brokerAddress({std::move(brokerUrl), brokerPort}),
+        m_state{StateInit} {
+        s_sharedResources.up();
     }
-}
 
-void MqttClient::printErrorMessage(int rc) {
-    std::cerr << "Error: " << std::string(mosqpp::strerror(rc)) << '\n';
-}
+    MqttClient::~MqttClient() {
+        s_sharedResources.down();
+    }
+
+    void MqttClient::connect() {
+        int rc;
+        if (m_state == StateDisconnected) {
+            rc = mosquittopp::reconnect();
+        } else {
+            rc = mosquittopp::connect_async(std::get<0>(m_brokerAddress).c_str(),
+                                            std::get<1>(m_brokerAddress),
+                                            s_keepAliveS);
+        }
+
+        if (rc) {
+            printErrorMessage(rc);
+            reconnect();
+        }
+    }
+
+    void MqttClient::reconnect() {
+        std::cerr << "Trying to reconnect in "
+                  << s_reconnectTimeoutS << " seconds\n";
+        utils::TimedAsyncExecution(s_reconnectTimeoutS*1000,
+                                   [this](){ connect(); });
+    }
+
+    void MqttClient::on_connect(int rc) {
+        if (!rc) {
+            std::cout << "Connected...\n";
+            m_state = StateConnected;
+        } else {
+            printErrorMessage(rc);
+            if (m_state != StateInit)
+                m_state = StateDisconnected;
+
+            reconnect();
+        }
+    }
+
+    void MqttClient::printErrorMessage(int rc) {
+        std::cerr << "Error: " << std::string(mosqpp::strerror(rc)) << '\n';
+    }
 
 } // namespace comm
--- server-side/data-collector/src/MqttClient.hpp
+++ server-side/data-collector/src/MqttClient.hpp
@@ -9,61 +9,61 @@
 
 namespace comm {
 
-class MqttClient : public mosqpp::mosquittopp {
-    private:
-        class SharedResources {
-            public:
-                SharedResources() noexcept : m_count(0) {};
-                void up();
-                void down();
+    class MqttClient : public mosqpp::mosquittopp {
+        private:
+            class SharedResources {
+                public:
+                    SharedResources() noexcept : m_count(0) {};
+                    void up();
+                    void down();
 
-            private:
-                std::mutex m_lock;
-                uint32_t m_count;
-        };
+                private:
+                    std::mutex m_lock;
+                    uint32_t m_count;
+            };
 
-        enum State {
-            StateInit,
-            StateConnected,
-            StateDisconnected
-        };
+            enum State {
+                StateInit,
+                StateConnected,
+                StateDisconnected
+            };
 
-    public:
-        class Exception : public std::exception {
-            public:
-                explicit Exception(std::string msg) :
-                    m_msg("MqttClient: " + std::move(msg)) {}
-                char const* what() const noexcept override {
-                    return m_msg.c_str();
-                }
+        public:
+            class Exception : public std::exception {
+                public:
+                    explicit Exception(std::string msg) :
+                        m_msg("MqttClient: " + std::move(msg)) {}
+                    char const* what() const noexcept override {
+                        return m_msg.c_str();
+                    }
 
-            private:
-                std::string m_msg;
-        };
+                private:
+                    std::string m_msg;
+            };
 
-    public:
-        MqttClient(std::string brokerUrl,
-                   uint16_t brokerPort,
-                   std::string clientId);
-        ~MqttClient();
+        public:
+            MqttClient(std::string brokerUrl,
+                       uint16_t brokerPort,
+                       std::string clientId);
+            ~MqttClient();
 
-        void connect();
+            void connect();
 
-    protected:
-        void on_connect(int rc) override;
+        protected:
+            void on_connect(int rc) override;
 
-    private:
-        void reconnect();
-        static void printErrorMessage(int rc);
+        private:
+            void reconnect();
+            static void printErrorMessage(int rc);
 
-    private:
-        const std::tuple<std::string, uint16_t> m_brokerAddress;
-        State m_state;
+        private:
+            const std::tuple<std::string, uint16_t> m_brokerAddress;
+            State m_state;
 
-        static const uint32_t s_keepAliveS = 120;
-        static const uint32_t s_reconnectTimeoutS = 30;
-        static SharedResources s_sharedResources; 
-};
+            static const uint32_t s_keepAliveS = 120;
+            static const uint32_t s_reconnectTimeoutS = 30;
+            static SharedResources s_sharedResources;
+    };
 
 } // namespace comm
 
