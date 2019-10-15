aa40f0e676948d2ee87ae300737f051582c791a3
Amnon Paz <pazamnon@gmail.com>
2019-10-15 21:54:24 +0300

    Utils: Add "fromString"

--- server-side/mqtt-client/src/Utils.hpp
+++ server-side/mqtt-client/src/Utils.hpp
@@ -1,6 +1,20 @@
 #ifndef UTILS_HPP_
 #define UTILS_HPP_
 
+#include <sstream>
+
 #define UNUSED(x) (void)(x)
+
+namespace utils {
+
+    template <typename T>
+    T fromString(std::string str) {
+        std::stringstream ss{str};
+        T t;
+        ss >> t;
+        return t;
+    }
+
+}
 
 #endif // UTILS_HP_
